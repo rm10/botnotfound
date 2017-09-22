dd800617ad9a50aae54d3b3c5b9dfc42482e84fa
Tiago Salvador <tiagoSalvador7@gmail.com>
2017-09-23 00:49:43 +0100

    Added hello and weather features

--- bot.js
+++ bot.js
@@ -1,45 +1,110 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+// Import the discord.js module
+const Discord = require('discord.js');
+var util = require('util')
+const auth = require('./auth.json');
+var DarkSky = require('forecast.io');
+const darkSkyAuth = require('./darkskyauth.json');
+const geocodingAuth = require('./geocoding.json');
+var NodeGeocoder = require('node-geocoder');
 
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ola':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Hello '${message.author.username}
-                });
-            break;
-            case 'info':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'im just a test version of a discord bot, made by a newbie'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+var options = {
+  provider: 'openmapquest',
+
+  // Optional depending on the providers
+  httpAdapter: 'https', // Default
+  apiKey: geocodingAuth.token, // for Mapquest, OpenCage, Google Premier
+  formatter: null         // 'gpx', 'string', ...
+};
+
+/*var geocoder = NodeGeocoder(options);
+
+geocoder.geocode("marinha grande", function(err, res) {
+  console.log(res);
+
+  var confidence = 0;
+  var index = 0;
+  for(var i = 0; i < res.length; i++) {
+    var obj = res[i];
+    if (obj.extra.confidence > confidence) {
+      confidence = obj.extra.confidence;
+      index = i;
+    }
+  }
+
+  var latitude, longitude;
+  latitude = res[index].latitude;
+  longitude = res[index].longitude;
+
+  darksky.get(latitude, longitude, function (err, res, data) {
+    if (err) throw err;
+    var temperature = ((data.currently.temperature - 32) * (5/9));
+    console.log(data.currently.summary + " with temperature of " + (Math.round(temperature * 10) / 10) + "ºC");
+  });
+});*/
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+// The token of your bot - https://discordapp.com/developers/applications/me
+const token = auth.token;
+
+var options = {
+  APIKey: darkSkyAuth.token,
+  timeout: 1000
+},
+darksky = new DarkSky(options);
+
+// The ready event is vital, it means that your bot will only start reacting to information
+// from Discord _after_ ready is emitted
+client.on('ready', () => {
+  console.log('I am ready!');
 });
+
+// Create an event listener for messages
+client.on('message', message => {
+  if (message.content.substring(0, 1) == '-') {
+    var args = message.content.substring(1).split(' ');
+    var cmd = args[0];
+
+    args = args.splice(1);
+    if (cmd === 'hello') {
+      // Send "pong" to the same channel
+      message.channel.send("hello to you too  " + message.author.toString() + "!");
+    }
+    else if (cmd === 'weather') {
+      var location = args[0];
+
+      if (location === undefined) {
+        message.channel.send("location invalid dude");
+      }
+      else {
+        geocoder.geocode(location, function(err, res) {
+          console.log(res);
+
+          var confidence = 0;
+          var index = 0;
+          for(var i = 0; i < res.length; i++) {
+            var obj = res[i];
+            if (obj.extra.confidence > confidence) {
+              confidence = obj.extra.confidence;
+              index = i;
+            }
+          }
+
+          var latitude, longitude;
+          latitude = res[index].latitude;
+          longitude = res[index].longitude;
+
+          darksky.get(latitude, longitude, function (err, res, data) {
+            if (err) throw err;
+            var temperature = ((data.currently.temperature - 32) * (5/9));
+            message.channel.send(data.currently.summary + " with temperature of " + (Math.round(temperature * 10) / 10) + "ºC");
+          });
+        });
+      }
+    }
+  }
+});
+
+// Log our bot in
+client.login(token);
