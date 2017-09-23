ab2ba31d256ea364c0fa6a0039ab8e1fd1239e89
Tiago Salvador <tiagoSalvador7@gmail.com>
2017-09-23 01:24:03 +0100

    Several fixes and improvements. Code cleanup

--- bot.js
+++ bot.js
@@ -1,58 +1,36 @@
-// Import the discord.js module
+// Import llibraries
 const Discord = require('discord.js');
-var util = require('util')
-const auth = require('./auth.json');
-var DarkSky = require('forecast.io');
-const darkSkyAuth = require('./darkskyauth.json');
-const geocodingAuth = require('./geocoding.json');
-var NodeGeocoder = require('node-geocoder');
+const Util = require('util')
+const DarkSky = require('forecast.io');
+const NodeGeocoder = require('node-geocoder');
+
+// Import auth files
+const Auth = require('./auth.json');
+const DarkSkyAuth = require('./darkskyauth.json');
+const GeocodingAuth = require('./geocoding.json');
 
 var options = {
   provider: 'openmapquest',
 
   // Optional depending on the providers
   httpAdapter: 'https', // Default
-  apiKey: geocodingAuth.token, // for Mapquest, OpenCage, Google Premier
-  formatter: null         // 'gpx', 'string', ...
+  apiKey: GeocodingAuth.token, // for Mapquest, OpenCage, Google Premier
+  formatter: null // 'gpx', 'string', ...
 };
 
 var geocoder = NodeGeocoder(options);
-
-/*geocoder.geocode("marinha grande", function(err, res) {
-  console.log(res);
-
-  var confidence = 0;
-  var index = 0;
-  for(var i = 0; i < res.length; i++) {
-    var obj = res[i];
-    if (obj.extra.confidence > confidence) {
-      confidence = obj.extra.confidence;
-      index = i;
-    }
-  }
-
-  var latitude, longitude;
-  latitude = res[index].latitude;
-  longitude = res[index].longitude;
-
-  darksky.get(latitude, longitude, function (err, res, data) {
-    if (err) throw err;
-    var temperature = ((data.currently.temperature - 32) * (5/9));
-    console.log(data.currently.summary + " with temperature of " + (Math.round(temperature * 10) / 10) + "ºC");
-  });
-});*/
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
 // The token of your bot - https://discordapp.com/developers/applications/me
-const token = auth.token;
+const token = Auth.token;
 
 var options = {
-  APIKey: darkSkyAuth.token,
-  timeout: 1000
-},
-darksky = new DarkSky(options);
+    APIKey: DarkSkyAuth.token,
+    timeout: 1000
+  },
+  darksky = new DarkSky(options);
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
@@ -69,37 +47,55 @@ client.on('message', message => {
     args = args.splice(1);
     if (cmd === 'hello') {
       // Send "pong" to the same channel
-      message.channel.send("hello to you too  " + message.author.toString() + "!");
-    }
-    else if (cmd === 'weather') {
-      var location = args[0];
+      message.channel.send("hello to you too " + message.author.toString() + "!");
+    } else if (cmd === 'weather') {
+      var location = "";
+
+      for (var i = 0; i < args.length; i++) {
+        var obj = args[i];
+        location += obj + " ";
+      }
+
+      console.log(location);
 
       if (location === undefined) {
-        message.channel.send("location invalid dude");
-      }
-      else {
-        geocoder.geocode(location, function(err, res) {
-          console.log(res);
+        message.channel.send("Location invalid my dude!");
+      } else if (location.startsWith('my ')) {
+        message.channel.send("Fuck you " + message.author.toString() + " :)");
+      } else {
+        geocoder.geocode(location.trim(), function(err, res) {
+          if (err) {
+            message.channel.send("Seems that location is invalid!");
+          } else {
+            console.log(res);
 
-          var confidence = 0;
-          var index = 0;
-          for(var i = 0; i < res.length; i++) {
-            var obj = res[i];
-            if (obj.extra.confidence > confidence) {
-              confidence = obj.extra.confidence;
-              index = i;
+            var confidence = 0;
+            var index = 0;
+            for (var i = 0; i < res.length; i++) {
+              var obj = res[i];
+              if (obj.extra.confidence > confidence) {
+                confidence = obj.extra.confidence;
+                index = i;
+              }
+            }
+
+            if (confidence <= 0.1) {
+              message.channel.send("Seems that location is invalid!");
+            } else {
+              var latitude, longitude;
+              latitude = res[index].latitude;
+              longitude = res[index].longitude;
+
+              darksky.get(latitude, longitude, function(err, res, data) {
+                if (err) {
+                  message.channel.send("Couldn't get information for that location!");
+                } else {
+                  var temperature = ((data.currently.temperature - 32) * (5 / 9));
+                  message.channel.send(data.currently.summary + " with temperature of " + (Math.round(temperature * 10) / 10) + "ºC");
+                }
+              });
             }
           }
-
-          var latitude, longitude;
-          latitude = res[index].latitude;
-          longitude = res[index].longitude;
-
-          darksky.get(latitude, longitude, function (err, res, data) {
-            if (err) throw err;
-            var temperature = ((data.currently.temperature - 32) * (5/9));
-            message.channel.send(data.currently.summary + " with temperature of " + (Math.round(temperature * 10) / 10) + "ºC");
-          });
         });
       }
     }
